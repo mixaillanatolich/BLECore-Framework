be478f9ace90efa31bcebd5c5b7716f955b77954
Mikhail Muravev <mixaillanatolich@gmail.com>
2020-02-10 21:41:44 +0300

    bg images

--- BLECore Example/DeviceListViewController.swift
+++ BLECore Example/DeviceListViewController.swift
@@ -17,14 +17,7 @@ class DeviceListViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
-    */
-
 }
